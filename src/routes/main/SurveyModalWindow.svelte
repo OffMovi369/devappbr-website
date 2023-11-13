f64fbc8593980ba103cae0bb3acc31832e2fb334
Lght3322 <lightplay577@gmail.com>
2023-11-13 23:27:56 +0300

    add modalwindow

--- src/routes/main/SurveyModalWindow.svelte
+++ src/routes/main/SurveyModalWindow.svelte
@@ -1,6 +1,7 @@
 <script>
+    import { imask } from '@imask/svelte';
     import BaseModal from "$lib/client/BaseModal.svelte";
-    import { fade } from "svelte/transition";
+    import modalsStore from "$lib/client/modalsStore";
     import quest1 from "$lib/assets/slides/quest1.png"
     import quest2 from "$lib/assets/slides/quest2.png"
     import quest3 from "$lib/assets/slides/quest3.png"
@@ -8,7 +9,18 @@
     import quest5 from "$lib/assets/slides/quest5.png"
     import quest6 from "$lib/assets/slides/quest6.png"
     import quest7 from "$lib/assets/slides/quest7.png"
-    export let sl= 0;
+    let sl = 0;
+
+
+    function accept({ detail: maskRef }) {
+        value = maskRef.value;
+    }
+    const options = { 
+		mask: '+{0}(000)000-00-00',
+        lazy: true,
+	};
+    let tel = '';
+
 
     let slides = [
         {   
@@ -16,16 +28,15 @@
             que:"У вас есть готовый дизайн-проект?",
             disc:"Мы можем разработать дизайн-проект под Ваши предпочтения, но также работаем и с готовым дизайном.",
             url:quest2,
+            value:null,
             questions:[
                 {
                     id:1,
                     answ:"Есть",
-                    status:true,
                 },
                 {
                     id:2,
                     answ:"Нет",
-                    status:true,
                 }
             ]
         },
@@ -34,21 +45,19 @@
             que:"Выберите тип приложения, который Вас интересует",
             disc:"",
             url:quest3,
+            value:null,
             questions:[
                 {
                     id:1,
                     answ:"Для бизнеса (B2B)",
-                    status:false,
                 },
                 {
                     id:2,
                     answ:"Для частных лиц (B2C)",
-                    status:false,
                 },
                 {
                     id:3,
                     answ:"Другое",
-                    status:false,
                 }
             ]
         },
@@ -57,16 +66,15 @@
             que:"За какой период нужно разработать приложение?",
             disc:"",
             url:quest4,
+            value:null,
             questions:[
                 {
                     id:1,
                     answ:"Срочная разработка (от 3 недель)",
-                    status:false,
                 },
                 {
                     id:2,
                     answ:"Обычная разработка (от 2 месяцев)",
-                    status:false,
                 }
             ]
         },
@@ -75,16 +83,15 @@
             que:"Есть ли у Вас техническое задание на разработку?",
             disc:"Если у Вас нет ТЗ на разработку, то наши специалисты составят его по Вашим предпочтениям",
             url:quest5,
+            value:null,
             questions:[
                 {
                     id:1,
                     answ:"Есть",
-                    status:false,
                 },
                 {
                     id:2,
                     answ:"Нет",
-                    status:false,
                 }
             ]
         },
@@ -93,26 +100,22 @@
             que:"Под какие платформы разрабатывается мобильное приложение?",
             disc:"",
             url:quest6,
+            value:null,
             questions:[
                 {
                     id:1,
                     answ:"Android и IOS",
-                    status:false,
                 },
                 {
                     id:2,
                     answ:"только IOS",
-                    status:false,
                 },
                 {
                     id:3,
                     answ:"только Android",
-                    status:false,
                 }
             ]
         },
-
-
     ]
 </script>
 <BaseModal title="Настройка платежной системы">
@@ -120,24 +123,24 @@
     
     <form action="">
         {#if sl == 0}
-            <div>
-                <img class="quest_img"  src="{quest1}" alt="">
-            </div>
-            <div class="survey_content" >
-                <div class="quest_content">
-                    
-                    <div class="">
-                        <p class="quest_title">Ответьте на 5 вопросов и получите расчет стоимости</p>
-                        <p class="quest_dis">Расчет произойдет автоматически, согласно нашим алгоритмам</p>
-                    </div>
-                    <button class="main_wt_btn" on:click={()=>{sl++}}>Начать</button>
+                <div>
+                    <img class="quest_img"  src="{quest1}" alt="">
                 </div>
-            </div>
+                <div class="survey_content" >
+                    <div class="quest_content">
+                        
+                        <div class="">
+                            <p class="quest_title">Ответьте на 5 вопросов и получите расчет стоимости</p>
+                            <p class="quest_dis">Расчет произойдет автоматически, согласно нашим алгоритмам</p>
+                        </div>
+                        <button class="main_wt_btn dd" on:click={()=>{sl++}}>Начать</button>
+                    </div>
+                </div>
         
         {:else}
             {#each slides as quest (quest.id)}
                 {#if sl==quest.id && quest.id!=6}
-                    <div class="">
+                    <div class="q">
                         <div>
                             <img class="quest_img"  src="{quest.url}" alt="">
                         </div>
@@ -150,10 +153,10 @@
                                 </div>
                                 <div class="questions">
                                     {#each quest.questions as answer (answer.id) }
-                                        <div class="">
-                                            <input type="radio" name="answers">
+                                        <label class="question" class:input_active={ answer.answ == quest.value}>
+                                            <input type="radio" name="answers" value="{answer.answ}" bind:group={quest.value}>
                                             <p class="main_sm">{answer.answ}</p>
-                                        </div>
+                                        </label>
                                     {/each}
                                 </div>
                                 <div class="next_quest">
@@ -173,11 +176,25 @@
             <div class="survey_content">
                 <div class="quest_content">
                     <div class="">
-                        <p class="quest_title">Ответьте на 5 вопросов и получите расчет стоимости</p>
-                        <p class="quest_dis">Расчет произойдет автоматически, согласно нашим алгоритмам</p>
+                        <p class="quest_title">Закрепите за собой скидку в размере 25.000₽ и получите расчет стоимости</p>
+                    </div>
+                    <div class="questions">
+                        <div class="row">
+                            <p class="main_sm">Имя</p>
+                            <input type="text" placeholder="Введите имя">
+                        </div>
+                        <div class="row">
+                            <p class="main_sm">Номер телефона</p>
+                            <input name="phone" type="tel" id="" required {tel}
+                                use:imask={options}
+                                on:accept={accept} placeholder="+7 000 00 00 ">
+                        </div>
                     </div>
                     <div class="">
-                        <button class="main_wt_btn">Начать</button>
+                        <button class="main_wt_btn ddd" type="submit">Получить скидку</button>
+                    </div>
+                    <div class="">
+                        <p class="post_sm">Отправляя заявку, вы соглашаетесь с Политикой Конфиденциальности</p>
                     </div>
                 </div>
             </div>
@@ -193,6 +210,20 @@
         justify-content: center;
         width: 100%;
     }
+    .survey_content{
+        height: 100%;
+    }
+    .dd{
+        margin-top: 32px;
+    }
+    .ddd{
+        margin-top: 10px;
+    }
+    .q{
+        display: flex;
+        flex-direction: column;
+        height: 552px;
+    }
     .quest_title{
         font-size: 24px;
         font-weight: 700;
@@ -201,6 +232,7 @@
         color: var(--white-color);
         margin-bottom: 8px;
     }
+
     .quest_dis{
         font-size: 18px;
         font-weight: 500;
@@ -210,7 +242,7 @@
         color: var(--white-color);
     }
     .quest_content{
-        height: 252px;
+        height: 100%;
         display: flex;
         justify-content: space-between;
         flex-direction: column;
@@ -229,4 +261,55 @@
         width: 100%;
         justify-content: center;
     }
+    .questions{
+        display: flex;
+        flex-direction: column;
+        row-gap: 12px;
+        margin: 24px 0px;
+        position: relative;
+    }
+    .question{
+        height: 100%;
+        padding: 12px 20px;
+        position: relative;
+        width: 100%;
+        border-radius: 100px;
+        border: 2px var(--white-color) solid;
+        display: flex;
+        column-gap: 10px;
+        align-items: center;
+    }
+    .question input{  
+        appearance: none;
+        border-radius: 50%;
+        width: 16px;
+        height: 16px;
+        border: 2px solid var(--white-color);
+        transition: 0.2s all linear;
+    }
+    .question input:checked{
+        background-color: var(--white-color);
+    }
+    .post_sm{
+        font-family: Manrope;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 19px;
+        letter-spacing: 0em;
+        color: rgba(171, 174, 181, 1);
+        margin-top: 20px;
+    }
+    .row{
+        display: flex;
+        flex-direction: column;
+        row-gap: 8px;
+    }
+    .row input{
+        padding: 10px 24px;
+        border-radius: 100px;
+        border: 2px solid rgba(255, 255, 255, 0.15);
+        height: 50px;
+        color: var(--white-color);
+    }
+    
 </style>
